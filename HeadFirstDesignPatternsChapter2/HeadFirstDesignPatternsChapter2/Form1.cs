1284797381762b06a9ef168bfe6d155ed131431a
jeroon <jeroon@hotmail.com>
2017-11-04 15:51:24 +0100

    finished

--- HeadFirstDesignPatternsChapter2/HeadFirstDesignPatternsChapter2/Form1.cs
+++ HeadFirstDesignPatternsChapter2/HeadFirstDesignPatternsChapter2/Form1.cs
@@ -12,9 +12,107 @@ namespace HeadFirstDesignPatternsChapter2
 {
 	public partial class Form1 : Form
 	{
+		static public EventHandler handler;
+		static public WeatherData StaticWeatherData;
+		CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay();
+
 		public Form1()
 		{
+			if (StaticWeatherData == null)
+				StaticWeatherData = new WeatherData();
+
 			InitializeComponent();
+			
+			
 		}
+
+		public void MeasurementsChanged()
+		{
+			handler?.Invoke(null, null);
+			Tuple<float, float, float> tup =  currentConditionsDisplay.ReturnValues();
+			Print(tup.Item1 + " " + tup.Item2 + " " +  tup.Item3);
+		}
+
+		void Print<T>(T msg)
+		{
+			richTextBox1.Text += msg.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MeasurementsChanged();
+		}
+	}
+	public abstract class Display
+	{
+		public Display()
+		{
+			Form1.handler += new EventHandler(MeasurementsUpdated);
+		}
+
+		public void OnDestroy()
+		{
+			Form1.handler -= MeasurementsUpdated;
+		}
+
+		internal abstract void MeasurementsUpdated(object sender, EventArgs e);
+	}
+	public class CurrentConditionsDisplay : Display
+	{
+		float temperature;
+		float humidity;
+		float pressure;
+		
+
+		public CurrentConditionsDisplay() : base()
+		{
+			
+		}
+
+		internal override void MeasurementsUpdated(object sender, EventArgs e)
+		{
+			temperature = Form1.StaticWeatherData.GetTemperature();
+			humidity = Form1.StaticWeatherData.GetHumidity();
+			pressure = Form1.StaticWeatherData.GetPressure();
+		}
+
+		public Tuple<float, float, float> ReturnValues()
+		{
+			return new Tuple<float, float, float>(temperature, humidity, pressure);
+
+		}
+	}
+	public class StatisticsDisplay : Display
+	{
+		internal override void MeasurementsUpdated(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	//public class ForecastDisplay : Display
+	//{
+
+	//}
+
+
+	public class WeatherData
+	{
+		float temperature;
+		float humidity;
+		float pressure;
+
+		public float GetTemperature()
+		{
+			return temperature=3;
+		}
+		public float GetHumidity()
+		{
+			return humidity=2;
+		}
+		public float GetPressure()
+		{
+			return pressure=1;
+		}
+
 	}
 }
